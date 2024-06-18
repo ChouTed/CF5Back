0e4379ea4509eee285734e83a071d7c56ff299bb
ChouTed <theodorechoundris@gmail.com>
2024-06-18 20:29:13 +0300

    Added

---
+++ index.js
@@ -0,0 +1,34 @@
+// Get the client
+import mysql from 'mysql2/promise';
+
+// Create the connection to database
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456789',
+  database: 'asoe',
+});
+
+// A simple SELECT query
+try {
+  const [results, fields] = await connection.query(
+    'SELECT * FROM `tables`'
+  );
+
+  console.log(results); // results contains rows returned by server
+  console.log(fields); // fields contains extra meta data about results, if available
+} catch (err) {
+  console.log(err);
+}
+
+// // Using placeholders
+// try {
+//   const [results] = await connection.query(
+//     'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+//     ['Page', 45]
+//   );
+
+//   console.log(results);
+// } catch (err) {
+//   console.log(err);
+// }
